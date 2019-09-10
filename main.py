7310b08809be8339b63d7aae1a593bd81d6f4241
danaldea <40248541+danaldea@users.noreply.github.com>
2019-09-10 14:38:35 +0300

    Update main.py
    1. moved end_point from the settings.py file to the main.py file. 
    2. passed end_point to the function within main.py
    3. more debug info for each step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 
+
 from globals import *
 
 from pattern_storage import pattern_storage
@@ -8,9 +9,12 @@ from pattern_recognition import pattern_recognition
 
 total_start = time.time()
 
+end_point = 32000
+
 # Determines the length of the data
 print("Data length:", data_length)
-
+print("Setpoint:",end_point)
+print("To be processed",data_length-end_point)
 samples = 0
 while end_point < data_length:
     average_line = all_data[:end_point]
@@ -21,23 +25,27 @@ while end_point < data_length:
 
     # Store the patterns found since now
     pattern_storage(average_line=average_line,
-                    pattern_array=pattern_array, performance_array=performance_array)
+                    pattern_array=pattern_array, performance_array=performance_array,end_point=end_point)
 
     # Determine the current pattern based on the last dots_for_pattern data
-    current_pattern(average_line=average_line, pattern_for_recognition=pattern_for_recognition)
+    current_pattern(average_line=average_line, pattern_for_recognition=pattern_for_recognition,end_point=end_point)
 
     # Perform the pattern recognition
     pattern_recognition(samples=samples,
                         pattern_array=pattern_array, performance_array=performance_array,
-                        pattern_for_recognition=pattern_for_recognition)
+                        pattern_for_recognition=pattern_for_recognition,end_point=end_point)
 
+    #TODO mama tata grigore
     # Get one more sample
     samples += 1
 
     accuracy_average = np.average(accuracy_array)
-    print("Backtested accuracy is", str(accuracy_average), "% after ", samples, "samples")
+    print("Backtested accuracy is", str(accuracy_average), "% after ", samples, "samples,",data_length-end_point,"remaning,",accuracy_array.__len__(),"patterns detected")
 
     end_point += 1
+    #print('Main Endpoint', end_point)
 
 total_end = time.time()
 print("Script took ", total_end - total_start, " seconds")
+print(file_name)
+print("Profit:",profit_array)
